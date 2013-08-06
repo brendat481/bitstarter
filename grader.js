659abadec77ab91b4284e1922fe9fb8b83b46299
Ubuntu <ubuntu@ip-172-31-46-162.us-west-2.compute.internal>
2013-08-06 10:49:48 +0000

    corrected updated grader.js added

--- grader.js
+++ grader.js
@@ -31,12 +31,12 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "shielded-ocean-7490.herokuapp.com";
 
 
 
 
-
-var assertFileExists = function(infine) {
+var assertFileExists = function(infile) {
 	var instr = infile.toString();
 	if(!fs.existsSync(instr)) {
 		console.log("%s does not exist. Exiting.", instr);
@@ -72,8 +72,9 @@ var clone = function(fn) {
 
 if(require.main == module) {
 	program
-		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-c, --checks <check_file>', 'checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'shielded-ocean-7490.herokuapp.com', clone(assertFileExists), URL_DEFAULT)
 		.parse(process.argv);
 	var checkJson = checkHtmlFile(program.file, program.checks);
 	var outJson = JSON.stringify(checkJson, null, 4);
