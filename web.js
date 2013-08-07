72337822623363d6557fe0704dba0cd7b850b22e
Ubuntu <ubuntu@ip-172-31-44-214.us-west-2.compute.internal>
2013-08-07 19:11:17 +0000

    app.use added to web.js

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require("express");
 var fs = require('fs');
 var htmlfile = "index.html";
 
-var app = express();
+var app = express.createServer(express.logger());
 
-
+app.use(express.static('/home/ubuntu/bitstarter'));
 
 app.get('/', function(request, response) {
 	var html =  fs.readFileSync(htmlfile).toString();  
